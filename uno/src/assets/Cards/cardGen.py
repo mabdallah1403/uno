14c2954fab1d964b04023040c4a1e4a8083726f8
Abbass Karnib <akarnib74@gmail.com>
2025-12-17 16:56:30 -0500

    deck creation validated, fix card turtle drawing to print card info

--- uno/src/assets/Cards/cardGen.py
+++ uno/src/assets/Cards/cardGen.py
@@ -1,34 +1,79 @@
-# should there be a flag for the type of card? like the wildcards?
-import random
-from random import choices
+from enum import Enum
+import turtle
 
+class Color(Enum):
+   RED = "Red"
+   BLUE = "Blue"
+   GREEN = "Green"
+   YELLOW = "Yellow"
 
+class Action(Enum):
+   REVERSE = "Reverse"
+   SKIP = "Skip"
+   DRAW2 = "+2"
 
-numberCards = ['0'] + ['1'] + ['2'] + ['3'] + ['4'] + ['5'] + ['6'] + ['7'] + ['8'] + ['9']
-wildCard = ['reverse'] + ['skip'] + ['+2'] + ['Swap Color'] + ['+4 Swap Color'] 
-colorCards = ['red'] + ['blue'] + ['green'] + ['yellow'] 
-drawPiles = [wildCard] + [colorCards] + [numberCards]
+class Wild(Enum):
+   SWAPCOLOR = "Swap Color"
+   DRAW4SWAPCOLOR = "+4 Swap Color"
+
 # max 7 cards per player,
-# 108 total cards,
-# numbers 0-9
+# 108 total cards
+# numbers 0-9 19 per color
 # 8 skips (2 of each color),
 # 8 reverse, 8 draw 2,
 # 4 draw 4 change color, 4 change color
 def generator():
-   i = 0
-   while i < 7:
-      print(choices(list(drawPiles),k=7))
-      print("\nnext player\n")
-      i += 1
+ card_list = []
+ for color in Color:
+    for i in range(19):
+      card_list.append(color.value + str(i))
+    for action in Action:
+      card_list.append(color.value + action.value)
+      card_list.append(color.value + action.value)
 
-generator()
+ for wild in Wild:
+    for i in range(4):
+      card_list.append(wild.value)
+    
+ return card_list
 
-class Cards(object):
-   redCount, blueCount, greenCount, yellowCount = 0,0,0,0
-   fullDeck = 108
+def validate_deck():
+   card_list = generator()
+   print(len(card_list)) 
+   color_dict = {
+      "Red": 0,
+      "Blue" : 0,
+      "Green" : 0,
+      "Yellow" : 0
+   }
 
+   for color in color_dict:
+      for card in card_list:
+         if color in card:
+            color_dict[color] += 1
 
+   print(color_dict)     
+#validate_deck()
 
-class playerCards(object):
-   def __init__(self):
-       self.cardsInHand = []
+def draw_card():
+   card_list = generator()
+
+   draw = turtle.Turtle()
+   length = 100
+   width = 150
+
+   draw.hideturtle()
+   
+   draw.forward(length)
+   draw.left(90)
+
+   draw.forward(width)
+   draw.left(90)
+
+   draw.forward(length)
+   draw.left(90)
+
+   draw.forward(width)
+
+   turtle.mainloop()
+draw_card()
